45ea47b3aeb09ce72b713d164238d4e514bb9931
Arya Aditiya <aryaaditiya15@gmail.com>
2024-12-30 22:02:26 +0700

    wip

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -18,11 +18,11 @@
                 </div>
             @endif
 
-            <div class="grid grid-cols-1 gap-y-6 sm:grid-cols-2 gap-x-4 lg:grid-cols-4 xl:gap-x-6">
+            <div class="grid grid-cols-2 gap-y-6 gap-x-4 lg:grid-cols-4 xl:gap-x-6">
                 @foreach($products as $product)
                     <a href="#" class="group border border-gray-100 rounded shadow p-4">
                         <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                            <img src="{{ asset('storage/'. $product->image) }}" class="w-full rounded h-80 object-center object-cover group-hover:opacity-75" alt="">
+                            <img src="{{ asset('storage/'. $product->image) }}" class="w-full rounded h-56 lg:h-80 object-center object-cover group-hover:opacity-75" alt="">
                         </div>
                         <div class="flex items-center justify-between mt-4">
                             <div class="self-center">
@@ -38,7 +38,7 @@
                                     @csrf
                                     <input type="hidden" name="product_id" value="{{ $product->id }}">
                                     <input type="hidden" name="qty" value="1">
-                                    <button type="submit" class="inline-flex justify-center py-4 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    <button type="submit" class="inline-flex justify-center p-3 lg:p-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
                                         </svg>
